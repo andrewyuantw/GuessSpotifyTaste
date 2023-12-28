9fb3718699493f9b7afbd6a346e8867bf6148589
Andrew Yuan <ayuan124@umd.edu>
2023-12-28 11:09:22 -0500

    Initial Nuxt Commit

---
+++ spotify_nuxt/app.vue
@@ -0,0 +1,11 @@
+<script>
+console.log(process.env.CLIENT_ID)
+console.log(process.env.CLIENT_SECRET)
+</script>
+
+<template>
+  <div>
+    <v-btn variant="outlined"> Button </v-btn>
+    
+  </div>
+</template>
